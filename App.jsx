98a717ccbbba8e793094b4f92cb60a4b1ad85174
meckurypro <meckurypro@gmail.com>
2026-07-29 21:39:03 +0100

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,21 @@
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import Services from './components/Services'
+import Cast from './components/Cast'
+import Community from './components/Community'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
+
+export default function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Services />
+      <Cast />
+      <Community />
+      <Contact />
+      <Footer />
+    </>
+  )
+}
