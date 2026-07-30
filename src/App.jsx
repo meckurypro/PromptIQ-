f3b78545318fa51050f184d4199b958aba329b4a
meckurypro <meckurypro@gmail.com>
2026-07-30 19:57:43 +0100

    Add Founder component to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Services from './components/Services'
 import Cast from './components/Cast'
+import Founder from './components/Founder'
 import Community from './components/Community'
 import Contact from './components/Contact'
 import Footer from './components/Footer'
@@ -13,6 +14,7 @@ export default function App() {
       <Hero />
       <Services />
       <Cast />
+      <Founder />
       <Community />
       <Contact />
       <Footer />
